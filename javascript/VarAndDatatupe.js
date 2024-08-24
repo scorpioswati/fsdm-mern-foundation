134d4de30148fc691da2ded40247ace786484280
Swati Panwar <scorpioswati11@gmail.com>
2024-08-24 23:06:30 +0530

    javascript

---
+++ javascript/VarAndDatatupe.js
@@ -0,0 +1,36 @@
+//Variables and Constant
+
+let variable = 'I am a variable'
+const constant = 'I am a constant'
+var legacyVariable = 'I am a legacy variable'
+
+
+console.log("varaianl", variable);
+
+//update 
+
+variable='swati'
+// constant='vinay'
+legacyVariable='shweta'
+
+console.log("varaianl", variable);
+// console.log("constant", constant);
+console.log("legacyVariable", legacyVariable);
+//primitive Data types
+
+let stringExample='hello javascript'
+console.log("stringExample", stringExample,typeof(stringExample));
+let number = 50
+
+console.log("number", number, typeof number);
+let booloean = true
+
+
+console.log("booloean", booloean, typeof booloean);
+//convert no. to string
+let num = 40
+let string = '10'
+let result = string+num
+console.log("result",result, typeof result)
+let convertedNumber = Number(result)
+console.log("convertedNumber", convertedNumber, typeof convertedNumber);
